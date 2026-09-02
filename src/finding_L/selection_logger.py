499a4881049a20945f16bfae6eed28cf7c367c6e
kait-ro <dabruanikait@gmail.com>
2026-09-03 03:20:54 +0530

    refactor: drop unused recordSelectionRound

--- src/finding_L/selection_logger.py
+++ src/finding_L/selection_logger.py
@@ -1,17 +1,4 @@
-import numpy as np
 import pandas as pd
-
-
-def recordSelectionRound(selectionLog: list, roundNumber: int, bestScore: float, residual: np.ndarray):
-    residualRms = np.sqrt(np.mean(residual ** 2))
-    selectionLog.append(
-        {
-            "round": roundNumber,
-            "bestReserveScore": bestScore,
-            "residualRms": residualRms,
-        }
-    )
-    return selectionLog
 
 
 def toSelectionLogFrame(selectionLog: list) -> pd.DataFrame:
